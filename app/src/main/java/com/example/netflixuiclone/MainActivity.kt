522935671e752d19fef758a0e17e9129fdf56ba7
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-26 14:02:33 +0530

    navController added

--- app/src/main/java/com/example/netflixuiclone/MainActivity.kt
+++ app/src/main/java/com/example/netflixuiclone/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.netflixuiclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.netflixuiclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+
     }
+    fun init() {
+        try {
+            val navControl = this.findNavController(R.id.nav_host_fragment_container)
+            binding.bottomNavBar.setupWithNavController(navControl)
+        }catch (_: Exception) {
+
+        }
+
+    }
+
 }
